1f72e9fbd71cb95bf8575659442e0abcd893a53d
robpuff <robbie9513@gmail.com>
2016-01-23 23:46:06 -0800

    Finally combined all 3 cameras into 1 cloud point

--- pc_combine/src/pc_combine.cpp
+++ pc_combine/src/pc_combine.cpp
@@ -11,12 +11,17 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
+using namespace std;
+
 ros::Publisher pub;
 bool cam1_data_valid = false;
 bool cam2_data_valid = false;
+bool cam3_data_valid = false;
 
-sensor_msgs::PointCloud2 output, output1, output2;
-pcl::PointCloud<pcl::PointXYZ> output_pcl, output1_pcl, output2_pcl;
+pcl::PointCloud<pcl::PointXYZ> input1_pcl, input2_pcl, input3_pcl;
+pcl::PointCloud<pcl::PointXYZ> output_pcl, output1_pcl, output2_pcl, output3_pcl;
+
+string base_link_id = "base_link";
 
 pcl::PCLPointCloud2 pcl_cam1, pcl_cam2, pcl_combined;
 tf::TransformListener *tf_listener;
@@ -25,9 +30,13 @@ void pcl_combine ()
 {
   cam1_data_valid = false;
   cam2_data_valid = false;
+  cam3_data_valid = false;
+
+  sensor_msgs::PointCloud2 output;
 
   output_pcl = output1_pcl;
   output_pcl += output2_pcl;
+  output_pcl += output3_pcl;
 
   pcl::toROSMsg(output_pcl, output);
   
@@ -38,17 +47,20 @@ void cloud_cb_cam1 (const sensor_msgs::PointCloud2ConstPtr& input)
 {
   cam1_data_valid = true;
   
-  tf_listener->waitForTransform("/base_link", (*input).header.frame_id, (*input).header.stamp, ros::Duration(5.0));
-  // Create a container for the data.
-  //sensor_msgs::PointCloud2 ros_cam1;
+  pcl::fromROSMsg(*input, input1_pcl);
+  tf::StampedTransform transform;
+  try
+  {
+	  tf_listener->lookupTransform(base_link_id, input->header.frame_id, ros::Time(0), transform);
+  }
+  catch (tf::TransformException ex)
+  {
+	  ROS_ERROR("%s", ex.what());
+  }
+  pcl_ros::transformPointCloud (input1_pcl, output1_pcl, transform);
+  output1_pcl.header.frame_id=base_link_id;
 
-  // Do data processing here...
-  pcl_ros::transformPointCloud("pf1_link", *input, output1, *tf_listener);
-  //pcl_conversions::toPCL(ros_cam1, pcl_cam1);
-  pcl::fromROSMsg(output1, output1_pcl);
-  // Publish the data.
-  //pub.publish (output);
-  if (cam1_data_valid && cam2_data_valid)
+  if (cam1_data_valid && cam2_data_valid && cam3_data_valid)
   {
     pcl_combine();
   }
@@ -57,18 +69,46 @@ void cloud_cb_cam1 (const sensor_msgs::PointCloud2ConstPtr& input)
 void cloud_cb_cam2 (const sensor_msgs::PointCloud2ConstPtr& input)
 {
   cam2_data_valid = true;
-  
-  tf_listener->waitForTransform("/base_link", (*input).header.frame_id, (*input).header.stamp, ros::Duration(5.0));
-  // Create a container for the data.
-  //sensor_msgs::PointCloud2 ros_cam2;
+  base_link_id = "base_link";
 
-  // Do data processing here...
-  pcl_ros::transformPointCloud("pf2_link", *input, output2, *tf_listener);
-  //pcl_conversions::toPCL(ros_cam2, pcl_cam2);
-  pcl::fromROSMsg(output2, output2_pcl);
-  // Publish the data.
-  //pub.publish (output);
-  if (cam1_data_valid && cam2_data_valid)
+  pcl::fromROSMsg(*input, input2_pcl);
+  tf::StampedTransform transform;
+  try
+  {
+	  tf_listener->lookupTransform(base_link_id, input->header.frame_id, ros::Time(0), transform);
+  }
+  catch (tf::TransformException ex)
+  {
+	  ROS_ERROR("%s", ex.what());
+  }
+  pcl_ros::transformPointCloud (input2_pcl, output2_pcl, transform);
+  output2_pcl.header.frame_id=base_link_id;
+
+  if (cam1_data_valid && cam2_data_valid && cam3_data_valid)
+  {
+	pcl_combine();
+  }
+}
+
+void cloud_cb_cam3 (const sensor_msgs::PointCloud2ConstPtr& input)
+{
+  cam3_data_valid = true;
+  base_link_id = "base_link";
+
+  pcl::fromROSMsg(*input, input3_pcl);
+  tf::StampedTransform transform;
+  try
+  {
+	  tf_listener->lookupTransform(base_link_id, input->header.frame_id, ros::Time(0), transform);
+  }
+  catch (tf::TransformException ex)
+  {
+	  ROS_ERROR("%s", ex.what());
+  }
+  pcl_ros::transformPointCloud (input3_pcl, output3_pcl, transform);
+  output3_pcl.header.frame_id=base_link_id;
+
+  if (cam1_data_valid && cam2_data_valid && cam3_data_valid)
   {
     pcl_combine();
   }
@@ -85,6 +125,7 @@ int main (int argc, char** argv)
   // Create a ROS subscriber for the input point cloud
   ros::Subscriber sub = nh.subscribe ("/pf1/points", 1, cloud_cb_cam1);
   ros::Subscriber sub2 = nh.subscribe ("/pf2/points", 1, cloud_cb_cam2);
+  ros::Subscriber sub3 = nh.subscribe("/pf3/points", 1, cloud_cb_cam3);
   
   // Create a ROS publisher for the output point cloud
   pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
