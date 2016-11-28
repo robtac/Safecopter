a6af481c4201d282e65d2d2e7c56c0255caace62
robpuff <robbie9513@gmail.com>
2016-11-27 22:21:51 -0800

    Added collision detection with FCL and octomap_server

--- safecopter/src/safecopter.cpp
+++ safecopter/src/safecopter.cpp
@@ -37,7 +37,7 @@
 
 using namespace std;
 
-ros::Publisher pub, new_direction_pub;
+ros::Publisher pub, new_direction_pub, visCubePub;
 bool cam1_data_valid = false;
 bool cam2_data_valid = false;
 bool cam3_data_valid = false;
@@ -61,7 +61,7 @@ void draw_new_direction (float rotation, float distance, bool willCollide)
 {
   visualization_msgs::Marker marker;
   // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-  marker.header.frame_id = "base_link";
+  marker.header.frame_id = base_link_id;
   marker.header.stamp = ros::Time::now();
 
   // Set the namespace and id for this marker.  This serves to create a unique ID
@@ -107,60 +107,99 @@ void draw_new_direction (float rotation, float distance, bool willCollide)
   new_direction_pub.publish(marker);
 }
 
+void drawCUBE(fcl::Vec3f vec, int c_color, fcl::Vec3f size)
+    {
+        visualization_msgs::Marker marker;
+        marker.header.frame_id = base_link_id;
+        marker.header.stamp = ros::Time();
+        marker.ns = "basic_shapes";
+        marker.id = 22;
+        marker.type = visualization_msgs::Marker::CUBE;
+        marker.action = visualization_msgs::Marker::ADD;
+        marker.pose.position.x = vec[0];
+        marker.pose.position.y = vec[1];
+        marker.pose.position.z = vec[2];
+        marker.pose.orientation.x = 0;
+        marker.pose.orientation.y = 0;//poseQ[1];
+        marker.pose.orientation.z = 0;//poseQ[2];
+        marker.pose.orientation.w = 1;//poseQ[3];
+
+        marker.scale.x = size[0];
+        marker.scale.y = size[1];
+        marker.scale.z = size[2];
+        marker.color.a = 1.0;
+        marker.color.r = 0.0;
+        marker.color.g = 0.0;
+        if(c_color == 0)
+        {
+            marker.color.r = 0.0;
+            marker.color.b = 0.0;
+            marker.color.g = 0.0;
+        }
+        if(c_color == 1)
+        {
+            marker.color.r = 0.0;
+            marker.color.b = 1.0;
+            marker.color.g = 0.0;
+        }
+        else if(c_color == 2)
+        {
+            marker.color.r = 1.0;
+            marker.color.b = 0.0;
+            marker.color.g = 0.0;
+        }
+        else
+        {
+            marker.color.r = 0.0;
+            marker.color.b = 0.0;
+            marker.color.g = 1.0;
+        }
+        //marker.lifetime = ros::Duration(0.3);
+        visCubePub.publish(marker);
+}
+
 bool detect_collision (float degree_theta)
 {
   float quadWidth = 0.8;
   float quadHeight = 0.4;
   bool willCollide = false;
-  int collidingPoints = 0;
 //  Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
-//  float theta = (M_PI / 180) * degree_theta;
+  float theta = (M_PI / 180) * degree_theta;
 //  float lowerX = min_distance; float lowerY = (-quadWidth / 2); float lowerZ = (-quadHeight / 2);
 //  float upperX = collision_distance; float upperY = (quadWidth / 2); float upperZ = (quadHeight / 2);
   
-//  std::cout << theta << std::endl << lowerX << " " << lowerY << " " << lowerZ << std::endl;
-//  std::cout << upperX << " " << upperY << " " << upperZ << std::endl;
-//  float lowerBoxX = (lowerX * cos (theta)) - (lowerY * sin (theta));
-//  float lowerBoxY = (lowerX * sin (theta)) + (lowerY * cos (theta));
-//  float lowerBoxZ = lowerZ;
-//  std::cout << lowerBoxX << " " << lowerBoxY << " " << lowerBoxZ << std::endl;
-//  Eigen::Vector3f lowerBoxCorner (lowerBoxX, lowerBoxY, lowerBoxZ);
-  
-//  float upperBoxX = (upperX * cos (theta)) - (upperY * sin (theta));
-//  float upperBoxY = (upperX * sin (theta)) + (upperY * cos (theta));
-//  float upperBoxZ = upperZ;
-//  std::cout << upperBoxX << " " << upperBoxY << " " << upperBoxZ << std::endl;
-//  Eigen::Vector3f upperBoxCorner (upperBoxX, upperBoxY, upperBoxZ);
-//  std::vector<int> indexVector;
-  
-//  pcl::PointXYZ searchPoint;
-//  searchPoint.x = 0;
-//  searchPoint.y = 0;
-//  searchPoint.z = 0;
-//  float radius = 1.0f;
-//  std::vector<int> pointIdxRadiusSearch;
-//  std::vector<float> pointRadiusSquaredDistance;
-  
-//  fcl::Box *box = new fcl::Box(.1, .1, .1); //arguments are size
-//  box->cost_density = 100; //?
-//  box->threshold_occupied = 5; //?
+  fcl::Vec3f size = fcl::Vec3f(collision_distance, quadWidth, quadHeight);
+  fcl::Box *box = new fcl::Box(size[0], size[1], size[2]); //arguments are size
+  box->cost_density = 100; //?
+  box->threshold_occupied = 5; //?
 
-//  //boost::shared_ptr<octomap::OcTree> ptr = octree;
-//  fcl::OcTree* tree = new fcl::OcTree(boost::shared_ptr<const octomap::OcTree>(octree));
+  //boost::shared_ptr<octomap::OcTree> ptr = octree;
+  fcl::OcTree* tree = new fcl::OcTree(boost::shared_ptr<const octomap::OcTree>(octree));
 
-//  fcl::CollisionObject * obj1 = new fcl::CollisionObject(boost::shared_ptr<fcl::CollisionGeometry>(box), fcl::Transform3f());
-//  fcl::CollisionObject * obj2 = new fcl::CollisionObject(boost::shared_ptr<fcl::CollisionGeometry>(tree), fcl::Transform3f());
+  fcl::Vec3f boxCenter = fcl::Vec3f(collision_distance / 2 + 0.2, 0, 0);
+  fcl::CollisionObject * collision_box = new fcl::CollisionObject(boost::shared_ptr<fcl::CollisionGeometry>(box), boxCenter);
+  fcl::CollisionObject * collision_tree = new fcl::CollisionObject(boost::shared_ptr<fcl::CollisionGeometry>(tree), fcl::Transform3f());
 
-//  fcl::CollisionRequest request;
-//  fcl::CollisionResult result;
+  fcl::CollisionRequest request;
+  fcl::CollisionResult result;
 
-//  fcl::collide(obj1, obj2, request, result);
+  fcl::collide(collision_box, collision_tree, request, result);
 
-//  delete obj1;
-//  delete obj2;
+  fcl::AABB b = collision_box -> getAABB();
+  fcl::Vec3f vec = b.center();
 
-//  std::cout << "NumContacts: " << result.numContacts() << std::endl;
+  if (result.isCollision() == true)
+  {
+      drawCUBE(vec, 0, size);
+  }
 
+//  delete collision_box;
+//  delete collision_tree;
+
+  if (result.numContacts() > 0)
+  {
+      willCollide = true;
+  }
   return willCollide;
 }
 
@@ -168,9 +207,8 @@ void avoid_collision ()
 {
   int max_collision_angle = 90;
   int value_to_add = 5;
-  int i = value_to_add;
   bool willCollide = true;
-  for (int degree = 5; degree <= max_collision_angle; degree = degree + i)
+  for (int degree = 0; degree <= max_collision_angle; degree = degree + value_to_add)
   {
     if (detect_collision(degree))
     {
@@ -184,18 +222,21 @@ void avoid_collision ()
       willCollide = false;
       break;
     }
-    
-    if (detect_collision(-degree))
+
+    if (degree != 0)
     {
-      //std::cout << "Collision detected" << std::endl;
-      draw_new_direction(-degree, 0.5, true);
-    }
-    else
-    {
-      //std::cout << "No collision" << std::endl;
-      draw_new_direction(degree, 4.0, false);
-      willCollide = false;
-      break;
+        if (detect_collision(-degree))
+        {
+          //std::cout << "Collision detected" << std::endl;
+          draw_new_direction(-degree, 0.5, true);
+        }
+        else
+        {
+          //std::cout << "No collision" << std::endl;
+          draw_new_direction(degree, 4.0, false);
+          willCollide = false;
+          break;
+        }
     }
   }
   
@@ -219,9 +260,9 @@ void colorize ()
     if (distance > 0 || distance < 0 || distance == 0) { // null test
       if (point.y < quadWidth / 2 && point.y > -quadWidth / 2 && distance < collision_distance && point.z < quadHeight / 2 && point.z > -quadHeight / 2 && distance > min_distance)
       {
-	point.r = 255;
-	point.g = 255;
-	point.b = 255;
+    point.r = 255;
+    point.g = 255;
+    point.b = 255;
 	
 	collidingPoints++;
 	if (collidingPoints > 20)
@@ -250,29 +291,15 @@ void colorize ()
     
     output_pcl.at(i) = point;
   }
-  
-  if (willCollide) 
-  {
-    
-    avoid_collision();
-  }
-  else
-  {
-    draw_new_direction(0, 4.0, false);
-    std::cout << "Nothing to avoid" << std::endl;
-  }
 }
 
 void callback_tree (const octomap_msgs::Octomap & msg){
-  //octree = octomap_msgs::binaryMsgToMap (msg);
+  octomap::AbstractOcTree* tree = octomap_msgs::binaryMsgToMap (msg);
+  octree = dynamic_cast<octomap::OcTree*>(tree);
   //octomap::OcTree tree (0.05);
-//  octomap::AbstractOcTree * abstract = octomap_msgs::binaryMsgToMap (msg);
-//  std::ostream s ( NULL );
-//  abstract -> write(s);
-//  octree -> read(s);
 
   //octree = boost::shared_ptr<octomap::OcTree>(tree);
-  colorize();
+  avoid_collision();
 }
 
 void pcl_combine ()
@@ -290,30 +317,32 @@ void pcl_combine ()
   output_pcl += output2_pcl;
   output_pcl += output3_pcl;
 
+  colorize();
+
   pcl::toROSMsg(output_pcl, output);
 
-  octomap::Pointcloud octPointCloud;
-  for (int i = 0; i < output_pcl.size(); i++)
-  {
-      pcl::PointXYZRGB point = output_pcl.at(i);
-      double distance = sqrt((point.x * point.x) + (point.y * point.y) + (point.z * point.z));
-      if (distance > 0 || distance < 0 || distance == 0) // null test
-      {
-          octomap::point3d endpoint (point.x, point.y, point.z);
-          octPointCloud.push_back(endpoint);
-      }
-  }
+//  octomap::Pointcloud octPointCloud;
+//  for (int i = 0; i < output_pcl.size(); i++)
+//  {
+//      pcl::PointXYZRGB point = output_pcl.at(i);
+//      double distance = sqrt((point.x * point.x) + (point.y * point.y) + (point.z * point.z));
+//      if (distance > 0 || distance < 0 || distance == 0) // null test
+//      {
+//          octomap::point3d endpoint (point.x, point.y, point.z);
+//          octPointCloud.push_back(endpoint);
+//      }
+//  }
 
-  octomap::point3d origin (0.0, 0.0, 0.0);
-  octree -> insertPointCloud (octPointCloud, origin);
+//  octomap::point3d origin (0.0, 0.0, 0.0);
+//  octree -> insertPointCloud (octPointCloud, origin);
 //  octree -> updateInnerOccupancy();
 
-  colorize();
+  //colorize();
 
   pub.publish(output);
   
   newTime = ros::Time::now();
-  std::cout << "Pcl_combine time: " << (newTime - firstTime) << std::endl;
+  //std::cout << "Pcl_combine time: " << (newTime - firstTime) << std::endl;
   std::cout << "Time since last point clould published: " << (newTime - oldTime) << std::endl;
   oldTime = newTime;
 }
@@ -417,11 +446,12 @@ int main (int argc, char** argv)
   ros::Subscriber sub = nh.subscribe ("/pf1/points", 1, cloud_cb_cam1);
   ros::Subscriber sub2 = nh.subscribe ("/pf2/points", 1, cloud_cb_cam2);
   ros::Subscriber sub3 = nh.subscribe("/pf3/points", 1, cloud_cb_cam3);
-  //ros::Subscriber subTree = nh.subscribe("/octomap_binary", 1, &callback_tree);
+  ros::Subscriber subTree = nh.subscribe("/octomap_binary", 1, &callback_tree);
   
   // Create a ROS publisher for the output point cloud
   pub = nh.advertise<sensor_msgs::PointCloud2> ("cloud_in", 1);
   new_direction_pub = nh.advertise<visualization_msgs::Marker>("new_direction", 1);
+  visCubePub = nh.advertise<visualization_msgs::Marker>("collision_box", 1);
   // Spin
   ros::spin ();
 }
