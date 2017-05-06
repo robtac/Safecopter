e79e3e48737bcab17a4da9cfabf7a22df68064e2
robpuff <robbie9513@gmail.com>
2017-05-06 00:17:07 -0700

    Added simple_avoid script to move back 1 meter when it detects an object

--- safecopter/scripts/simple_avoid.py
+++ safecopter/scripts/simple_avoid.py
@@ -5,15 +5,13 @@ import math
 import rosbag
 import time
 import tf
-import tf2_ros
-import tf2_geometry_msgs
 
 from numpy import linalg
 import numpy as np
 
 from visualization_msgs.msg import Marker
 from std_msgs.msg import Header, Float32, Bool
-from geometry_msgs.msg import PoseStamped, Quaternion, TransformStamped
+from geometry_msgs.msg import PoseStamped, Quaternion
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
 from mavros_msgs.srv import CommandLong
 from sensor_msgs.msg import NavSatFix
@@ -43,42 +41,29 @@ class MavrosOffboardPosctlTest():
         self.has_global_pos = False
         self.local_position = PoseStamped()
         self.armed = False
+
         self.direction_from_collision = 0
         self.can_find_path = False
         self.will_collide = False
-        #self.t = tf2_ros.TransformListener()
+        self.t = tf.TransformListener()
 
     #
     # General callback functions used in tests
     #
     def position_callback(self, data):
         self.local_position = data
-        br = tf2_ros.TransformBroadcaster()
-        t = TransformStamped()
-
-        t.header.stamp = rospy.Time.now()
-        t.header.frame_id = "odom"
-        t.child_frame_id = "base_link"
-        t.transform.translation.x = data.pose.position.x
-        t.transform.translation.y = data.pose.position.y
-        t.transform.translation.z = data.pose.position.z
-        t.transform.rotation.x = data.pose.orientation.x
-        t.transform.rotation.y = data.pose.orientation.y
-        t.transform.rotation.z = data.pose.orientation.z
-        t.transform.rotation.w = data.pose.orientation.w
-
-        br.sendTransform(t)
-        # br.sendTransform((data.pose.position.x, data.pose.position.y, 0),
-        #                  (data.pose.orientation.w, data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z),
-        #                  rospy.Time.now(),
-        #                  "base_link",
-        #                  "odom")
+        br = tf.TransformBroadcaster()
+        br.sendTransform((data.pose.position.x, data.pose.position.y, 0),
+                         (data.pose.orientation.w, data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z),
+                         rospy.Time.now(),
+                         "base_link",
+                         "odom")
 
     def global_position_callback(self, data):
         self.has_global_pos = True
 
     def safecopter_direction_callback(self, msg):
-        self.direction_from_collision = msg.data
+        self.safecopter_direction = msg.data
 
     def can_find_path_callback(self, msg):
         self.can_find_path = msg.data
@@ -161,29 +146,25 @@ class MavrosOffboardPosctlTest():
 
         base_pos = self.create_pose(x, y, z, 0, 0, 0, "base_link")
 
-        tfBuffer = tf2_ros.Buffer()
-        listener = tf2_ros.TransformListener(tfBuffer)
+        tf_listener = tf.TransformListener()
         pos = base_pos
-        # try:
-        trans = tfBuffer.lookup_transform("odom", "base_link", rospy.Time())
-        pos = tf2_geometry_msgs.do_transform_pose(pos, trans)
+        try:
+            ros_time = rospy.Time.now()
+            tf_listener.waitForTransform("odom", "base_link", ros_time, rospy.Duration(4.0))
+            pos = self.t.transformPose("odom", base_pos)
+        except tf.Exception:
+            pass
 
-            # ros_time = rospy.Time.now()
-            # tf_listener.waitForTransform("odom", "base_link", ros_time, rospy.Duration(4.0))
-            # pos = self.t.transformPose("odom", base_pos)
-        # except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-        #     print("Lookup Transformation Error")
-
-        #x_difference = pos.pose.position.x - self.local_position.pose.position.x
-        #y_difference = pos.pose.position.y - self.local_position.pose.position.y
-        #yaw = 0
-        #if y_difference != 0:
-        #    if x_difference < 0:
-        #        yaw = math.atan2(y_difference, x_difference)
-        #    else:
-        #        yaw = math.atan2(y_difference, x_difference)
-        #quat = quaternion_from_euler(0, 0, yaw)
-        #pos.pose.orientation = Quaternion(*quat)
+        x_difference = pos.pose.position.x - self.local_position.pose.position.x
+        y_difference = pos.pose.position.y - self.local_position.pose.position.y
+        yaw = 0
+        if y_difference != 0:
+            if x_difference < 0:
+                yaw = math.atan2(y_difference, x_difference)
+            else:
+                yaw = math.atan2(y_difference, x_difference)
+        quat = quaternion_from_euler(0, 0, yaw)
+        pos.pose.orientation = Quaternion(*quat)
 
         return pos
 
@@ -195,45 +176,23 @@ class MavrosOffboardPosctlTest():
         pos.pose.position.x = x
         pos.pose.position.y = y
         pos.pose.position.z = z
-        x_difference = pos.pose.position.x - self.local_position.pose.position.x
-        y_difference = pos.pose.position.y - self.local_position.pose.position.y
-        # yaw facing towards end target
+        # yaw facing forwards
         yaw = 0
-        if y_difference != 0:
-            if x_difference < 0:
-                yaw = math.atan2(y_difference, x_difference)
-            else:
-                yaw = math.atan2(y_difference, x_difference)
         quaternion = quaternion_from_euler(0, 0, yaw)
         pos.pose.orientation = Quaternion(*quaternion)
-
-        turned_local_pos = PoseStamped()
-        turned_local_pos.header = Header()
-        turned_local_pos.header.frame_id = "base_footprint"
-        turned_local_pos.pose.position.x = self.local_position.pose.position.x
-        turned_local_pos.pose.position.y = self.local_position.pose.position.y
-        turned_local_pos.pose.position.z = 10
-        turned_local_pos.pose.orientation = Quaternion(*quaternion)
 
         # does it reach the position in X seconds?
         count = 0
         while count < timeout:
             # update timestamp for each published SP
             pos.header.stamp = rospy.Time.now()
-            if self.is_at_yaw(yaw, 10):
-                print("Can find path: " + str(self.can_find_path) + " -- Will collide: " + str(self.will_collide))
-                if self.local_position.pose.position.z > 2.5:
-                    if self.can_find_path:
-                        if self.will_collide:
-                            self.pub_spt.publish(self.get_temp_pos(1))
-                            # self.stop()
-                        else:
-                            # FIXME: safe mode for when no path is found
-                            self.pub_spt.publish(pos)
-                else:
-                    self.pub_spt.publish(pos)
+            print("Can find path: " + str(self.can_find_path) + " -- Will collide: " + str(self.will_collide))
+            if self.will_collide:
+                pos.pose.position.x = -1
+                self.pub_spt.publish(pos)
             else:
-                self.pub_spt.publish(turned_local_pos)
+                pos.pose.position.x = 0
+                self.pub_spt.publish(pos)
 
             if not self.armed and count > 5:
                 self._srv_cmd_long(False, 176, False,
@@ -247,8 +206,8 @@ class MavrosOffboardPosctlTest():
 
                 self.armed = True
 
-            if self.is_at_position(pos.pose.position.x, pos.pose.position.y, pos.pose.position.z, 1):
-                break
+            # if self.is_at_position(pos.pose.position.x, pos.pose.position.y, pos.pose.position.z, 1):
+            #     break
             count += 1
             self.rate.sleep()
 
@@ -256,21 +215,9 @@ class MavrosOffboardPosctlTest():
         while not self.has_global_pos:
             self.rate.sleep()
 
-        # positions = (
-        #     (0, 0, 2),
-        #     (2, 2, 2),
-        #     (2, -2, 2),
-        #     (-2, -2, 2),
-        #     (2, 2, 2))
+        position = (0, 0, 1)
 
-        positions = (
-            (0, 0, 10),
-            (10, 0, 10),
-            (0, 0, 10))
-        print(positions)
-
-        for i in range(0, len(positions)):
-            self.reach_position(positions[i][0], positions[i][1], positions[i][2], 1000)
+        self.reach_position(position[0], position[1], position[2], 1000)
 
 
 if __name__ == '__main__':
